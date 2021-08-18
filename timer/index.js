837e7e06f90aa94bd6cd6e53a130545a016e67f2
Lisgevan <episimo@hotmail.com>
2021-08-18 20:27:04 +0300

    Declared Start and Pause methods

--- timer/index.js
+++ timer/index.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+class Timer {
+	constructor(durationInput, startButton, pauseButton) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+
+		this.startButton.addEventListener("click", this.start);
+		this.pauseButton.addEventListener("click", this.pause);
+	}
+	//declare start with arrow function so that "this" will refer to he class and not the window
+	start = () => {
+		//call tick manual to start immediately
+		this.tick();
+		//then run tick() every second
+		this.interval = setInterval(this.tick, 1000);
+	};
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
+
+	tick = () => {
+		console.log("ticked");
+	};
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
