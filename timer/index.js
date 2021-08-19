d41b1dafc549e56c216d5730add795bd26ba858c
Lisgevan <episimo@hotmail.com>
2021-08-19 19:20:59 +0300

    draw circle and prepare animation

--- timer/index.js
+++ timer/index.js
@@ -1,7 +1,12 @@
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
 
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
 	//callback functions to check when the timer started, when it is ticking, when it is complete
 	// we will be using them to change the web page instead of putting the code in the our class
@@ -9,7 +14,8 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
 		console.log("Timer started");
 	},
 	onTick() {
-		console.log("Timer ticked");
+		circle.setAttribute("stroke-dashoffset", currentOffset);
+		currentOffset -= 50;
 	},
 	onComplete() {
 		console.log("Timer completed");
