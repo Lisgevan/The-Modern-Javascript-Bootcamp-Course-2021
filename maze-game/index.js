e79bf37959efd0e4784cb88e30a82af7f4114162
Lisgevan <episimo@hotmail.com>
2021-08-22 20:43:02 +0300

    prepared for maze creation function

--- maze-game/index.js
+++ maze-game/index.js
@@ -1,7 +1,8 @@
 //get access to objects from Matter library by destructuring them
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const cells = 3;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -18,31 +19,56 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-	world,
-	MouseConstraint.create(engine, {
-		mouse: Mouse.create(render.canvas),
-	})
-);
-
 //walls
 const walls = [
-	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-	Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
-	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 
 World.add(world, walls);
 
-//Random Shapes
-for (let i = 0; i < 50; i++) {
-	if (Math.random() > 0.5) {
-		World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-	} else {
-		World.add(
-			world,
-			Bodies.circle(Math.random() * width, Math.random() * height, 35, { render: { fillStyle: "red" } })
-		);
-	}
-}
+//Maze generation
+
+// const grid = Array(3)
+// 	.fill(null)
+//  .map( () => Array( 3 ).fill( false ) );//create an array with 3 items and each item to be an array of 3 items with the value of false
+
+//grid array
+const grid = Array(cells)
+	.fill(null)
+	.map(() => Array(cells).fill(false));
+//vertical walls
+const verticals = Array(cells)
+	.fill(null)
+	.map(() => Array(cells - 1).fill(false));
+//horizontal walls
+const horizontals = Array(cells - 1)
+	.fill(null)
+	.map(() => Array(cells).fill(false));
+
+//pick  random cell
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+	//If I have visited the cell at [row, column], then return
+	//code here
+	//Mark this cell as being visited
+	//code here
+	//Assemble randomly-ordered list of neighbors
+	//code here
+	//For each neighbor...
+	//code here
+	//...see if that neighbor is out of bounds
+	//code here
+	//...see if we have visited that neighbor, continue to next one
+	//code here
+	//...remove a wall from either horizontals or verticals
+	//code here
+	//...visit that next cell
+	//code here
+};
+
+stepThroughCell(startRow, startColumn);
