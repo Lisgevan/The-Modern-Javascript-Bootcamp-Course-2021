ca43905fc4c06fd16d2a1c4d8903397fa5370ddb
Lisgevan <episimo@hotmail.com>
2021-08-23 15:41:57 +0300

    added starting and ending object, movement, wining function

--- maze-game/index.js
+++ maze-game/index.js
@@ -1,13 +1,14 @@
 //get access to objects from Matter library by destructuring them
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 3;
+const cells = 10;
 const width = 600;
 const height = 600;
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
 	element: document.body,
@@ -23,10 +24,10 @@ Runner.run(Runner.create(), engine);
 
 //walls
 const walls = [
-	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+	Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 2, height, { isStatic: true }),
 ];
 
 World.add(world, walls);
@@ -130,7 +131,10 @@ horizontals.forEach((row, rowIndex) => {
 			rowIndex * unitLength + unitLength, //calculate y axis distance
 			unitLength, //width of wall
 			10, //height of wall
-			{ isStatic: true }
+			{
+				label: "wall",
+				isStatic: true,
+			}
 		);
 		World.add(world, wall);
 	});
@@ -146,9 +150,65 @@ verticals.forEach((row, rowIndex) => {
 			columnIndex * unitLength + unitLength, //calculate x axis distance
 			rowIndex * unitLength + unitLength / 2, //calculate y axis distance to the center of wall
 			10, //width of wall
-			unitLength,
-			{ isStatic: true } //height of wall
+			unitLength, //height of wall
+			{
+				label: "wall",
+				isStatic: true,
+			}
 		);
 		World.add(world, wall);
+	});
+});
+
+//Placing starting and ending points
+//ending point
+const goal = Bodies.rectangle(
+	width - unitLength / 2,
+	height - unitLength / 2,
+	(unitLength * 60) / 100,
+	(unitLength * 60) / 100,
+	{
+		isStatic: true,
+		render: { fillStyle: "green" },
+		label: "goal",
+	}
+);
+World.add(world, goal);
+
+//starting point
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength * 0.2, {
+	render: { fillStyle: "red" },
+	label: "ball",
+});
+World.add(world, ball);
+
+document.addEventListener("keydown", event => {
+	const { x, y } = ball.velocity;
+	if (event.code === "KeyW" || event.code === "ArrowUp") {
+		Body.setVelocity(ball, { x, y: y - 5 });
+	}
+	if (event.code === "KeyD" || event.code === "ArrowRight") {
+		Body.setVelocity(ball, { x: x + 5, y });
+	}
+	if (event.code === "KeyS" || event.code === "ArrowDown") {
+		Body.setVelocity(ball, { x, y: y + 5 });
+	}
+	if (event.code === "KeyA" || event.code === "ArrowLeft") {
+		Body.setVelocity(ball, { x: x - 5, y });
+	}
+});
+
+//Win condition
+Events.on(engine, "collisionStart", event => {
+	event.pairs.forEach(collision => {
+		const labels = ["ball", "goal"];
+		if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+			world.gravity.y = 1;
+			world.bodies.forEach(body => {
+				if (body.label === "wall") {
+					Body.setStatic(body, false);
+				}
+			});
+		}
 	});
 });
