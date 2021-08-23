b5cc5ad1197fe0e986a5b18b8d68dafb7ecf6c66
Lisgevan <episimo@hotmail.com>
2021-08-23 11:37:45 +0300

    finished the maze creation function

--- maze-game/index.js
+++ maze-game/index.js
@@ -29,11 +29,24 @@ const walls = [
 
 World.add(world, walls);
 
-//Maze generation
+// ==> Maze generation <==
 
 // const grid = Array(3)
 // 	.fill(null)
 //  .map( () => Array( 3 ).fill( false ) );//create an array with 3 items and each item to be an array of 3 items with the value of false
+
+//shuffle array function
+const shuffle = arr => {
+	let counter = arr.length;
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+		counter--;
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
+	}
+	return arr;
+};
 
 //grid array
 const grid = Array(cells)
@@ -54,19 +67,48 @@ const startColumn = Math.floor(Math.random() * cells);
 
 const stepThroughCell = (row, column) => {
 	//If I have visited the cell at [row, column], then return
-	//code here
+	if (grid[row][column]) {
+		return;
+	}
+
 	//Mark this cell as being visited
-	//code here
+	grid[row][column] = true;
+
 	//Assemble randomly-ordered list of neighbors
-	//code here
+	const neighbors = shuffle([
+		[row - 1, column, "up"], //above
+		[row, column + 1, "right"], //right
+		[row + 1, column, "down"], //bottom
+		[row, column - 1, "left"], //left
+	]);
+
 	//For each neighbor...
-	//code here
-	//...see if that neighbor is out of bounds
-	//code here
-	//...see if we have visited that neighbor, continue to next one
-	//code here
-	//...remove a wall from either horizontals or verticals
-	//code here
+	for (let neighbor of neighbors) {
+		const [nextRow, nextColumn, direction] = neighbor;
+
+		//...see if that neighbor is out of bounds
+		if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+			continue;
+		}
+
+		//...see if we have visited that neighbor, continue to next one
+		if (grid[nextRow][nextColumn]) {
+			continue;
+		}
+
+		//...remove a wall from either horizontals or verticals
+		if (direction === "left") {
+			verticals[row][column - 1] = true;
+		} else if (direction === "right") {
+			verticals[row][column] = true;
+		} else if (direction === "up") {
+			horizontals[row - 1][column] = true;
+		} else if (direction === "down") {
+			horizontals[row][column] = true;
+		}
+		stepThroughCell(nextRow, nextColumn);
+	}
+
 	//...visit that next cell
 	//code here
 };
