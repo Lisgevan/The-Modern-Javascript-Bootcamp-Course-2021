f2ca83b4d1cfd8c5408d120e209124306e3c0e91
Lisgevan <episimo@hotmail.com>
2021-08-29 09:49:15 +0300

    added styling and external css file

--- 05-E-Commerce/index.js
+++ 05-E-Commerce/index.js
@@ -4,7 +4,9 @@ const authRouter = require("./routes/admin/auth");
 
 //const bodyParser = require("body-parser"); (====>depreciated way)
 const app = express();
+
 // the .use(middlewareFunction) is used when we want all our wrap handlers (like app) to have that middleware we applied
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cookieSession({
